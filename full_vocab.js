1a72c4110c24255e4c1565987ff6918ff181c04b
Alex <103360320+alex1-0@users.noreply.github.com>
2025-07-04 23:30:54 +0200

    Update full_vocab.js

--- full_vocab.js
+++ full_vocab.js
@@ -1,4 +1,4 @@
-const vocabulary = [
+window.fullVocabulary = [
     { type: "lesson", number: 1 },
     { latin: "ecce", stems: "", translation: "Siehe! Schaut!" },
     { latin: "ibi", stems: "Adv.", translation: "dort" },
